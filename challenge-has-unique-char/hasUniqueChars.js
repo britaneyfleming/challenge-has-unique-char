8a7d98507da5f45f4daf88425ade66201221a584
Britaney Fleming <britaneyrfleming@gmail.com>
2022-11-09 14:58:09 -0600

    final commit

--- challenge-has-unique-char/hasUniqueChars.js
+++ challenge-has-unique-char/hasUniqueChars.js
@@ -1,1 +1,23 @@
+//Given a word, return true if that word contains only unique characters. Return false otherwise.
+
+//For example:
+
+//hasUniqueChars("Monday")
+// returns true
+
+//hasUniqueChars("Moonday")
+// returns false
+
+//Uppercase and lowercase letters should be considered separately:
+//hasUniqueChars("Bob")
+// returns true
+
 // Write your code below
+
+function uniqueChars(word){
+    let unique = new Set([])
+    for(let i = 0; i < word.length; i++){
+    unique.add(word[i])
+}
+return unique.size === word.length
+}
